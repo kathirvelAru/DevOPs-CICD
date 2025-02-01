ec10578af77feb977bfaeb0714130fa5fe0e193e
kathirvelAru <159253642+kathirvelAru@users.noreply.github.com>
2025-02-01 15:39:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 // Define a route
 app.get('/', (req, res) => {
-res.send('Congratulations HHLD Folks - we are in ECS Demo version 2 yahhhhh!');
+res.send('Congratulations HHLD Folks - we are in ECS Demo version 2 yahhhhh!changes reflected from github');
 });
 
 
